6252183987dd02c4cc66b96b65eccc4d09dc4b38
Mel23sek <163374605+Mel23sek@users.noreply.github.com>
2024-03-20 16:31:58 +0000

    add server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 app.use(express.static(path.join(__dirname)));
 
