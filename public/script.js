ac24b5212389e39f1ab3f459deb993a42fadd9dd
Mel23sek <163374605+Mel23sek@users.noreply.github.com>
2024-03-22 03:26:30 +0000

    add public

--- public/script.js
+++ public/script.js
@@ -139,7 +139,7 @@ function updateLocalStorage() {
       delete window.structuredAnswers;
   }
 }
-const SERVERLESS_ENDPOINT = 'https://tftportal.com/api/submit_quiz';
+const SERVERLESS_ENDPOINT = 'submit_quiz.js';
 
 function submitQuiz(gradeLevel, longAnswer) {
   const userName = localStorage.getItem('userName');
